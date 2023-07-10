482b223ed185ea79fad333220e25ffd5caf3454f
Mansi Jadhav <mansij.3112@gmail.com>
2023-07-10 17:15:22 -0500

    make contents centered

--- src/components/LetterPage.jsx
+++ src/components/LetterPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import ProgressBar from './ProgressBar';
-function LetterPage(){
-    return (
-        <div className='grad-page-body'>
-            <h1 className='text-6xl font-bold tracking-tight'> Lydia, Welcome to Loyola!</h1>
-            <h3 className='text-3xl font-medium pt-8 subheading pb-18'>Access your decision letter and Post Admit Portal.</h3>
-            <ProgressBar/>
-        </div>
-    )
+import React from "react";
+import ProgressBar from "./ProgressBar";
+
+function LetterPage() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="grad-page-body">
+        <h1 className="text-6xl font-bold tracking-tight">
+          Lydia, Welcome to Loyola!
+        </h1>
+        <h3 className="text-3xl font-medium pt-8 subheading pb-18">
+          Access your decision letter and Post Admit Portal.
+        </h3>
+        <ProgressBar />
+      </div>
+    </div>
+  );
 }
 
 export default LetterPage;
