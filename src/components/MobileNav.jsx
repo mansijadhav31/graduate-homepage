1d0cfbba766d0b9fb52425c7e20b8e84ede48eaf
Mansi Jadhav <mansij.3112@gmail.com>
2023-07-10 17:16:28 -0500

    remove mobile navigation

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { toggleResponsiveClass } from '../functions';
+import React, { useState } from 'react';
 import { Fade as Hamburger } from 'hamburger-react';
 
-export function MobileNav({toggled, toggle}){
-    return (
-        <div>
-           <div className="header-logo">
-          <a href="www.google.com" className="active">
-            <img
-              src="https://gpem-luc-edu.cdn.technolutions.net/www/images/GPEM_Website/LUC_stacked.png"
-              height="40px"
-              width="auto"
-              alt="Logo"
-            />
-          </a>
-        </div>
-        <div>
-        <Hamburger toggled={toggled} toggle={toggle} />
-        </div>
-        </div>
-    );
+export function MobileNav() {
+  const [isToggled, setToggled] = useState(false);
+
+  const handleToggle = () => {
+    setToggled(!isToggled);
+  };
+
+  return (
+    <div className="flex justify-between items-center mobile-header-wrapper">
+      <div className="mobile-header-logo">
+        <a href="www.google.com" className="active">
+          <img
+            src="https://gpem-luc-edu.cdn.technolutions.net/www/images/GPEM_Website/LUC_stacked.png"
+            height="20px"
+            width="auto"
+            alt="Logo"
+          />
+        </a>
+      </div>
+      <div>
+        <Hamburger toggled={isToggled} toggle={handleToggle} />
+      </div>
+    </div>
+  );
 }
